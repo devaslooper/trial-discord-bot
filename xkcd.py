25a34508f836d5d3c7b911e440b050f87764b3e0
Venkata Mukund Kashyap Yedunuthala <mukundkashyapyn@outlook.com>
2020-07-28 10:05:38 +0200

    test build 2

--- xkcd.py
+++ xkcd.py
@@ -12,6 +12,8 @@ if xkcd.status_code == 200:
     alt_text = comic.img["title"]
     content = "XKCD "+str(index)+f"\n{alt_text}\n{comic_url}"
     disc_url = os.getenv("XKCD_WH")
+    if disc_url[0] !='h':
+        disc_url='https://'+disc_url
     requests.post(disc_url,data={"content":f"{content}"})
     index+=1
     with open("xkcdlog.json","w+") as opfile:
